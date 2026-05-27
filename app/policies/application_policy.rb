47bc7c00ed9f2fd0f634a4634fcae89a36e23170
Freivin Campbell <freivin12@gmail.com>
2026-05-26 20:05:08 -0600

    Set up Rails application foundation

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  def activate?
+    false
+  end
+
+  def deactivate?
+    false
+  end
+
+  def export?
+    false
+  end
+
+  def manage?
+    false
+  end
+
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      raise NoMethodError, "You must define #resolve in #{self.class}"
+    end
+
+    private
+
+    attr_reader :user, :scope
+  end
+end
