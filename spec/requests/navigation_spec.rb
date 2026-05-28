c9559592c26de2d65eb21f63b7faa2ec625bb18f
Freivin Campbell <freivin12@gmail.com>
2026-05-27 20:34:56 -0600

    Add authenticated navigation menu

---
+++ spec/requests/navigation_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+RSpec.describe "Navigation" do
+  it "shows the platform menu and logout to super admins" do
+    user = create(:user, :super_admin)
+
+    sign_in user
+
+    get root_path
+
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include("Plataforma")
+    expect(response.body).to include("Mis iglesias")
+    expect(response.body).to include("Cerrar sesion")
+  end
+
+  it "shows church admin navigation to church owners" do
+    church = create(:church)
+    membership = create(:church_membership, :owner, church:)
+
+    sign_in membership.user
+
+    get church_admin_ministries_path(church)
+
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include("Resumen")
+    expect(response.body).to include("Roles")
+    expect(response.body).to include("Usuarios")
+    expect(response.body).to include("Miembros")
+    expect(response.body).to include("Ministerios")
+    expect(response.body).to include("Cerrar sesion")
+  end
+
+  it "does not show logout to guests" do
+    get root_path
+
+    expect(response).to have_http_status(:ok)
+    expect(response.body).not_to include("Cerrar sesion")
+  end
+
+  it "logs users out from the navigation action" do
+    user = create(:user)
+
+    sign_in user
+
+    delete destroy_user_session_path
+
+    expect(response).to redirect_to(root_path)
+  end
+end
